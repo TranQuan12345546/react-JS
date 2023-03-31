ca24f41b8c424d397fae764518208e15c05ebf3a
TranQuan12345546 <99300656+TranQuan12345546@users.noreply.github.com>
2023-03-31 10:21:28 +0700

    Update ColorBox.js

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,19 +1,45 @@
 import react, {useState} from "react";
 
 function ColorBox() {
-    const [shape, setShape] = useState("square");
-    const [color, setColor] = useState("deeppink");
+    const colors = [
+        '#3498db',
+        '#9b59b6',
+        '#e74c3c',
+        '#2c3e50',
+        '#d35400',
+    ]
+    const [colorss, setColorss] = useState(colors);
 
-    function handleBoxClick() {
-        setColor("green");
+    const handleBox = () => {
+        setColorss([...colorss, ...colors])
     }
 
+    const handleClick = (e) => {
+        let colorss2 = [];
+        colorss2 = colorss.filter(function(color, index) {
+            return index != e.target.className;
+        })
+        setColorss(colorss2);
+    }
+        
+ 
     return (
-        <div
-            className="color-box"
-            style={{ backgroundColor: color , width : '100px', height : '100px' }}
-            onClick={handleBoxClick}
-        ></div>
+        <div className="wrap">
+            <h1> JS DOM</h1>
+            <button id="btn" onClick={handleBox}>More boxes</button>
+            <h4 id="score"> Total box: <span className="points">{colorss.length}</span></h4>
+
+            <div className="boxes">
+                {colorss.map((color, index) => (
+                    <div onClick={handleClick} className = {index} key={index} 
+                    style={{backgroundColor: color, width:'100px', height:'100px', float: 'left'}}>
+
+                    </div>
+                ))}
+
+                
+            </div>
+        </div>
     );
 }
 export default ColorBox;
